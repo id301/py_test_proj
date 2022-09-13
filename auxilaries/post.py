2295df6a635a7fc8c06adab3ea1aef992266d0cc
id301 <mam.nik.al@gmail.com>
2022-09-13 22:20:09 +0300

    added fields-existing validation

---
+++ auxilaries/post.py
@@ -0,0 +1,4 @@
+
+
+def is_field_in_post(post, field):
+        return field in post
--- tests/test_something.py
+++ tests/test_something.py
@@ -1,8 +1,10 @@
 ## https://my-json-server.typicode.com/typicode/demo/posts
 import requests
+import json
 
 from configuration import SERVICE_URL
 from src.enums.global_enums import GlobalErrorMessages
+from auxilaries import post
 
 def test_getting_posts():
     response = requests.get(url=SERVICE_URL)
@@ -10,3 +12,9 @@ def test_getting_posts():
 
     assert response.status_code == 200, GlobalErrorMessages.WRONG_STATUS_CODE.value
     assert len(received_posts) == 3, GlobalErrorMessages.WRONG_ELEMENT_COUNT.value
+    assert post.is_field_in_post(received_posts[0], "id"), GlobalErrorMessages.EXPECTED_FIELD_NOT_EXISTS.value + " - id"
+    assert post.is_field_in_post(received_posts[0], "title"), GlobalErrorMessages.EXPECTED_FIELD_NOT_EXISTS.value + " - title"
+
+
+#def is_field_in_post(post, field):
+#    return field in post
